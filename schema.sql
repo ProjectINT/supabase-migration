a872b77b8ffabb0543b7fd4d77e94b5ca73426d4
Yuri <admin@somnium.su>
2025-03-21 19:41:49 +0400

    🚀 Successful schema push
    successful schema push

--- schema.sql
+++ schema.sql
@@ -1,43 +1,52 @@
-alter database postgres
-set timezone to 'UTC';
+-- 1. Устанавливаем часовой пояс
+ALTER DATABASE postgres SET timezone TO 'UTC';
 
--- 1. Создание общих ролей и схемы
-CREATE SCHEMA common; -- Общая схема
+-- 2. Создаём общую схему и роль для неё
+CREATE SCHEMA common; -- Общая схема для всех групп
 CREATE ROLE common_user NOLOGIN; -- Роль для доступа к общей схеме
 
--- 2. Создание групп и их схем
-CREATE ROLE group1 NOLOGIN;      -- Компания 1
+-- 3. Создаём группы (NOLOGIN) и схемы
+CREATE ROLE group1 NOLOGIN;      -- Группа 1 (компания 1)
+CREATE ROLE group2 NOLOGIN;      -- Группа 2 (компания 2)
 CREATE SCHEMA group1_schema;     -- Схема для группы 1
-CREATE ROLE group2 NOLOGIN;      -- Компания 2
 CREATE SCHEMA group2_schema;     -- Схема для группы 2
 
--- 3. Настройка привилегий для групп
--- Для Group1
+-- 4. Создаём технических пользователей-администраторов групп
+CREATE USER group1_admin WITH PASSWORD 'group1password';
+CREATE USER group2_admin WITH PASSWORD 'group2password';
+
+-- 5. Назначаем администраторов владельцами схем
+ALTER SCHEMA group1_schema OWNER TO group1_admin;
+ALTER SCHEMA group2_schema OWNER TO group2_admin;
+
+-- 6. Связываем администраторов с группами
+GRANT group1 TO group1_admin;
+GRANT group2 TO group2_admin;
+
+-- 7. Настройка привилегий для групп
+-- Группа 1
 GRANT USAGE ON SCHEMA group1_schema TO group1;
-
 GRANT CREATE, USAGE ON SCHEMA group1_schema TO group1;
-ALTER SCHEMA group1_schema OWNER TO group1;
 
--- Для Group2
+-- Группа 2
 GRANT USAGE ON SCHEMA group2_schema TO group2;
 GRANT CREATE, USAGE ON SCHEMA group2_schema TO group2;
-ALTER SCHEMA group2_schema OWNER TO group2;
 
--- 4. Настройка доступа к общей схеме
+-- 8. Настройка доступа к общей схеме
 GRANT USAGE ON SCHEMA common TO common_user;
 GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA common TO common_user;
 
--- 5. Связывание групп с общей схемой
+-- 9. Связываем группы с общей схемой
 GRANT common_user TO group1, group2;
 
--- 6. Создание конкретных пользователей
+-- 10. Создаём пользователей и назначаем им группы
 CREATE USER user1 WITH PASSWORD 'password1';
 GRANT group1 TO user1;
 
 CREATE USER user2 WITH PASSWORD 'password2';
 GRANT group2 TO user2;
 
--- 7. Отзыв прав у PUBLIC
+-- 11. Ограничиваем публичный доступ
 REVOKE ALL ON SCHEMA group1_schema FROM PUBLIC;
 REVOKE ALL ON SCHEMA group2_schema FROM PUBLIC;
 REVOKE ALL ON SCHEMA common FROM PUBLIC;
