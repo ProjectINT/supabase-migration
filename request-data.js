ea2d44ba12a706b15678a463a6ea1aef08f880e7
Yuri <admin@somnium.su>
2025-03-22 19:10:46 +0400

    ✅ Update test
    update test

--- request-data.js
+++ request-data.js
@@ -1,16 +1,23 @@
 
 // request-data.js
 import sql from './db.js';
+
 /*
   Это падает. Для 3000 пользователей не возможно вернуть все данные за раз.
 
+  Очень не понятно, то зависает то с ошибкой падает.
 */
+
+function hasNullByte(str) {
+  return /\x00/.test(str);
+}
+
 async function getUsersOver() {
   const users = await sql`
-    SELECT id FROM users
+    SELECT id FROM users LIMIT 300 OFFSET 300;
   `
 
-  console.log('users', users);
+  console.log('users', users.length);
   
   return users
 }
@@ -18,12 +25,18 @@ async function getUsersOver() {
 const usersData = await getUsersOver();
 
 usersData.forEach(async (user) => {
+  // Check input data
+  if (!hasNullByte(user.id)) {
+    console.log('user.id', user.id);
+  }
 
-  console.log('user.id', user.id);
-  const orders = await sql`
-    SELECT * FROM orders WHERE user_id = ${user.id} LIMIT 15
-  `;
-
-  console.log('orders', orders.length);
+  try {
+    const orders = await sql`
+      SELECT * FROM orders WHERE user_id = ${user.id} LIMIT 15;
+    `;
+    console.log('orders', orders.length);
+  } catch (error) {
+    console.error('Error', error);
+  }
 })
 
